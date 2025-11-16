1e3568080cbbc2926c201dc8252194bd9bb256c6
Keonne Gladden (KeKnows) <gladdenk.24@william.jewell.edu>
2025-11-16 15:11:01 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from backend.routes import auth
+from backend.routes import workout
 
-app = FastAPI()
 
 app.include_router(auth.router, prefix="/auth")
+app.include_router(workout.router)
+app = FastAPI()
